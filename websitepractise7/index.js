201ee24c3535c5b3c8bd37517bbb966eae1faaf2
NaumaanIpac96 <141617471+NaumaanIpac96@users.noreply.github.com>
2023-08-15 20:53:51 +0530

    Add files via upload

---
+++ websitepractise7/index.js
@@ -0,0 +1,107 @@
+// CREATING PORTFOLIO TABBED COMPONENT 
+
+console.log(`barca`); 
+
+const p_btns = document.querySelector(".p-btns"); 
+const p_btn = document.querySelectorAll(".p-btn");
+const p_img_elem = document.querySelectorAll(".img-overlay"); 
+
+p_btns.addEventListener("click" , (e) => {
+    const p_btn_clicked = e.target; 
+    console.log(p_btn_clicked); 
+
+
+    p_btn.forEach((curElem) => curElem.classList.remove("p-btn-active"))
+
+    p_btn_clicked.classList.add("p-btn-active")
+
+
+
+    // TO FIND NUMBER IN DATA 
+
+    const btn_num = p_btn_clicked.dataset.btnNum; 
+    console.log(btn_num); 
+
+
+
+    const img_active = document.querySelectorAll(`.p-btn--${btn_num}`)
+
+
+    p_img_elem.forEach((curElem) => curElem.classList.add("p-image-not-active"))
+
+    img_active.forEach((curElem) =>
+     curElem.classList.remove("p-image-not-active")); 
+
+}); 
+
+
+
+
+// SCROLL TO TOP BUTTON 
+
+const heroSection = document.querySelector(".section-hero")
+const footerElem = document.querySelector(".section-footer")
+
+
+const scrollElement = document.createElement("div"); 
+scrollElement.classList.add("scrollTop-style"); 
+
+scrollElement.innerHTML = `<ion-icon name="arrow-up-outline" class="scroll-top"></ion-icon>`
+
+
+footerElem.after(scrollElement); 
+
+const scrollTop = () => {
+  heroSection.scrollIntoView({behavior: "smooth"})
+
+}
+
+scrollElement.addEventListener("click" , scrollTop)
+
+
+
+
+
+
+
+
+// ANIMATE NUMBERS 
+
+
+
+ const counterNum = document.querySelectorAll(".counter-numbers"); 
+ const speed = 200;  counterNum.forEach((curElem) => {
+
+ const updateNumber = () =>{
+   const targetNumber = parseInt(curElem.dataset.number)
+  //  console.log(targetNumber);
+   const initialNum = parseInt(curElem.innerText)
+  //  console.log(initialNum)
+
+   const incrementNumber = Math.trunc(targetNumber / speed)
+  //  console.log(incrementNumber); 
+
+   if (initialNum < targetNumber){
+    curElem.innerText = `${initialNum + incrementNumber}+`; 
+
+    setTimeout(updateNumber , 10); 
+
+   }
+ }
+
+ updateNumber(); 
+
+})
+
+
+
+
+
+
+
+
+
+
+
+
+ 
